402e80c76da81e7f60f39b83b5306b6735a58eb5
madelene99 <madelene991144@gmail.com>
2018-09-21 14:17:05 +0300

    first

---
+++ 1/index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('2tm7yd8n') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			
